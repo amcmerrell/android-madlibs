4fbd17079051f28bad310689882f7075c0ceb45d
Andrew Fisher and Andrew Merrell <andrew@>
2016-11-15 11:58:36 -0800

    add tests for backend, started UI testing

---
+++ app/src/androidTest/java/com/epicodus/madlibs/MainActivityInstrumentationTest.java
@@ -0,0 +1,36 @@
+package com.epicodus.madlibs;
+
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class MainActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.nationality)).perform(typeText("Canadians"))
+                .check(matches(withText("Canadians")));
+    }
+
+    @Test
+    public void nationalityIsSentToResultsActivity() {
+        String nationality = "Canadians";
+        onView(withId(R.id.nationality)).perform(typeText(nationality));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.resultsTextView)).check(matches
+                (withText("All " + nationality + " are . They bring . We need to keep those clowns away from our . The only way to do that is to build a yuge . Also we need to get rid of that super loser . Have you seen their . Hey, if my wasn’t related to me I’d them.")));
+    }
+}
